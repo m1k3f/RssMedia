a7661b0c7e1633f5e26cef39e4514efb7271388c
m1k3f <41029862+mikedr0p@users.noreply.github.com>
2020-09-22 09:54:20 -0500

    bug fix to app.js

--- ClientApp/src/App.js
+++ ClientApp/src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { NavMenu } from './components/NavMenu';
+import { Content } from './components/Content';
 import './css/rssmedia.css'
 
 export default class App extends Component {
