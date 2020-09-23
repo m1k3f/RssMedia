81ff748646457c5ffa412f245786c7a4bb9f7038
m1k3f <41029862+mikedr0p@users.noreply.github.com>
2020-09-23 16:38:42 -0500

    started spinner logic

--- ClientApp/src/components/FeedArticles.js
+++ ClientApp/src/components/FeedArticles.js
@@ -10,6 +10,9 @@ export class FeedArticles extends Component {
                     list of title containers expandable to show content
                     button to load more articles from service
                 */}
+                {/* <div class="divSpinner">
+                    <i class="fas fa-spinner fa-spin fa-lg"></i>
+                </div> */}
                 <details name="feedArticles" className="detailsFeedArticles">
                     <summary>Test Article Summary 1</summary>
                     <article>Lorem ipsum dolor sit amet, consectetur adipiscing elit. In auctor, purus non condimentum egestas, felis sapien auctor sem, at laoreet lectus justo at purus. Nulla accumsan ligula eu nisl consectetur, sit amet fermentum elit tincidunt. Aenean a vulputate tellus. Mauris accumsan nunc vitae leo hendrerit, vel laoreet neque volutpat. Cras ornare egestas arcu, condimentum lacinia velit tristique quis. Proin congue erat elit, eget pulvinar leo sagittis eget. Nullam eros leo, convallis ac tortor quis, gravida dignissim lectus. Fusce vel ex sed ex scelerisque ultricies. Vivamus tempus facilisis sem id hendrerit. In id ex placerat, aliquam libero eu, tempus dui. Proin tempus gravida magna, sit amet faucibus orci suscipit et. Mauris fringilla tortor a sagittis auctor. Sed molestie euismod turpis, vitae iaculis magna lacinia at. </article>
