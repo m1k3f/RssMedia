a9f6855d2c0301820de3e96a92955562b5b5c18d
m1k3f <41029862+mikedr0p@users.noreply.github.com>
2020-10-20 16:16:33 -0500

    bug fix in control display; added sync

--- ClientApp/src/components/FeedArticlesControls.js
+++ ClientApp/src/components/FeedArticlesControls.js
@@ -1,6 +1,14 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
 
 export class FeedArticlesControls extends Component {
+
+    state = {
+        showControls: false
+    }
+
+    handleSyncClick = (e) => {
+        this.props.feedArticlesCallback("sync");
+    }
 
     handleEditClick = (e) => {
         this.props.feedArticlesCallback("edit");
@@ -10,26 +18,57 @@ export class FeedArticlesControls extends Component {
         this.props.feedArticlesCallback("delete");
     }
 
-    render() {
+    renderControls = () => {
         let hideDiv = (this.props.feedTitle.length === 0);
-
-        return(
-            <div className="divFeedArticlesControls" hidden={hideDiv}>
-                {/* 
-                    Feed Title (left)
-                    Edit Feed button (right)
-                    Delete Feed button (right)
-                */}
-                <p>{this.props.feedTitle}</p>
-                <div>
-                    <a className="clickable" onClick={this.handleEditClick}>
-                        <i className="fas fa-edit fa-lg"></i>
-                    </a>
-                    <a className="clickable" onClick={this.handleDeleteClick}>
-                        <i className="fas fa-trash fa-lg"></i>
-                    </a>
+        let content = null;
+        if (!hideDiv) {
+            content = (
+                <div className="divFeedArticlesControls">
+                    {/* 
+                        Feed Title (left)
+                        Edit Feed button (right)
+                        Delete Feed button (right)
+                    */}
+                    <p>{this.props.feedTitle}</p>
+                    <div>
+                        <a className="clickable" onClick={this.handleSyncClick}>
+                            <i className="fas fa-sync-alt fa-lg"></i>
+                        </a>
+                        <a className="clickable" onClick={this.handleEditClick}>
+                            <i className="fas fa-edit fa-lg"></i>
+                        </a>
+                        <a className="clickable" onClick={this.handleDeleteClick}>
+                            <i className="fas fa-trash fa-lg"></i>
+                        </a>
+                    </div>
                 </div>
-            </div>
+            );
+        }
+
+        return(content);
+    }
+
+    render() {
+        return(
+            <React.Fragment>
+                {this.renderControls()}
+            </React.Fragment>
+            // <div className="divFeedArticlesControls">
+            //     {/* 
+            //         Feed Title (left)
+            //         Edit Feed button (right)
+            //         Delete Feed button (right)
+            //     */}
+            //     <p>{this.props.feedTitle}</p>
+            //     <div>
+            //         <a className="clickable" onClick={this.handleEditClick}>
+            //             <i className="fas fa-edit fa-lg"></i>
+            //         </a>
+            //         <a className="clickable" onClick={this.handleDeleteClick}>
+            //             <i className="fas fa-trash fa-lg"></i>
+            //         </a>
+            //     </div>
+            // </div>
         );
     }
 }
