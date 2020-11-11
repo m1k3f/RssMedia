eb58e48142f5a0cfd872d669ea9e0f4555714fca
m1k3f <41029862+mikedr0p@users.noreply.github.com>
2020-11-11 16:55:10 -0600

    added animation to sync button

--- ClientApp/src/components/FeedArticles/Controls/SyncButton.js
+++ ClientApp/src/components/FeedArticles/Controls/SyncButton.js
@@ -2,14 +2,30 @@ import React, { Component } from 'react';
 
 export class SyncButton extends Component {
 
+    state = {
+        spin: false
+    }
+
     handleSyncClick = (e) => {
+        this.animateButton(true);
         this.props.controlsCallback("sync");
+        this.animateButton(false);
+    }
+
+    animateButton = (spin) => {
+        this.setState({
+            spin: spin
+        });
     }
 
     render() {
+        let iconClass = (this.state.spin) ? 
+                        'fas fa-sync-alt fa-spin fa-lg' : 
+                        'fas fa-sync-alt fa-lg';
+
         return(
             <a className="clickable" onClick={this.handleSyncClick}>
-                <i className="fas fa-sync-alt fa-lg"></i>
+                <i className={iconClass}></i>
             </a>
         );
     }
