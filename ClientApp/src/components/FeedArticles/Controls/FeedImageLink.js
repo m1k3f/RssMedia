0c167544108313102e93fb9a30a7f5f94e3f4651
m1k3f <41029862+mikedr0p@users.noreply.github.com>
2020-11-16 16:45:31 -0600

    logic to account for YT feed image

--- ClientApp/src/components/FeedArticles/Controls/FeedImageLink.js
+++ ClientApp/src/components/FeedArticles/Controls/FeedImageLink.js
@@ -2,17 +2,28 @@ import React, { Component} from 'react';
 
 export class FeedImageLink extends Component {
 
-    render() {
-        //let homepageLink = this.props.feed.;
-        let imageUrl = (this.props.selectedFeed.feedImageUrl !== '') 
-                        ? decodeURIComponent(this.props.selectedFeed.feedImageUrl) 
-                        : '';
+    renderImage = () => {
+        let image = '';
+        if (this.props.selectedFeed.feedRssUrl.includes("youtube.com")) {
+            image = (
+                <i className="fab fa-youtube"></i>
+            );            
+        }
+        else if (this.props.selectedFeed.feedImageUrl != null && this.props.selectedFeed.feedImageUrl.length > 0) {
+            let imageUrl = decodeURIComponent(this.props.selectedFeed.feedImageUrl);            
+            image = (
+                <img src={imageUrl} alt="" />
+            );
+        }
+        
+        return (image);
+    }
 
-        return(
-            // <a href={homepageLink}>
-            //     <img src={imageUrl} />
-            // </a>
-            <img src={imageUrl} alt="" />
+    render() {
+        return (
+            <React.Fragment>
+                {this.renderImage()}
+            </React.Fragment>
         );
     }
 }
