3d75e3ff2945c7164150e4f23114f676cd68b63f
m1k3f <41029862+mikedr0p@users.noreply.github.com>
2020-11-27 13:11:36 -0600

    hide all feeds edit & delete buttons

--- ClientApp/src/components/FeedArticles/FeedArticlesControls.js
+++ ClientApp/src/components/FeedArticles/FeedArticlesControls.js
@@ -13,18 +13,28 @@ export class FeedArticlesControls extends Component {
 
     renderControls = () => {
         let hideDiv = (this.props.feed == null);
+        let isAllFeeds = (this.props.feed != null && this.props.feed.feedName === 'All Feeds');
         let content = null;
         if (!hideDiv) {
-            // let imageUrl = (this.props.feed.feedImageUrl != '') ? decodeURIComponent(this.props.feed.feedImageUrl) : '';
-            content = (
-                <div className="divFeedArticlesControls">
-                    <FeedImageLink selectedFeed = {this.props.feed} />
-                    <SyncButton controlsCallback = {this.handleButtonClick} />
-                    <EditButton controlsCallback = {this.handleButtonClick} 
-                                selectedFeed = {this.props.feed} />
-                    <DeleteButton controlsCallback = {this.handleButtonClick} />
-                </div>
-            );
+            if (isAllFeeds) {
+                content = (
+                    <div className="divFeedArticlesControls">
+                        <FeedImageLink selectedFeed = {this.props.feed} />
+                        <SyncButton controlsCallback = {this.handleButtonClick} />
+                    </div>
+                );
+            }
+            else {
+                content = (
+                    <div className="divFeedArticlesControls">
+                        <FeedImageLink selectedFeed = {this.props.feed} />
+                        <SyncButton controlsCallback = {this.handleButtonClick} />
+                        <EditButton controlsCallback = {this.handleButtonClick} 
+                                    selectedFeed = {this.props.feed} />
+                        <DeleteButton controlsCallback = {this.handleButtonClick} />
+                    </div>
+                );
+            }
         }
 
         return(content);
