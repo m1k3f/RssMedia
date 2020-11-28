80585cdb897e69c2bbd5351fe1f5e4820a351e7b
m1k3f <41029862+mikedr0p@users.noreply.github.com>
2020-11-28 00:22:03 -0600

    feed image bug fix

--- ClientApp/src/components/FeedArticles/Controls/FeedImageLink.js
+++ ClientApp/src/components/FeedArticles/Controls/FeedImageLink.js
@@ -5,8 +5,7 @@ export class FeedImageLink extends Component {
     renderImage = () => {
         let image = '';
         let selectedFeed = this.props.selectedFeed;
-        if (selectedFeed === null || selectedFeed.feedImageUrl.length === 0 || 
-            selectedFeed.feedRssUrl.length === 0) {
+        if (selectedFeed === null || selectedFeed.feedRssUrl.length === 0) {
             image = (
                 <i className="fas fa-rss"></i>
             ); 
