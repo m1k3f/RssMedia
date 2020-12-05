91386471974b958003999babc8426294c8a18d64
m1k3f <41029862+mikedr0p@users.noreply.github.com>
2020-12-04 19:22:01 -0600

    added logic to resolve enclosure url

--- ClientApp/src/components/FeedArticles/modals/EnclosureLink.js
+++ ClientApp/src/components/FeedArticles/modals/EnclosureLink.js
@@ -4,45 +4,79 @@ import withReactContent from 'sweetalert2-react-content'
 
 export class EnclosureLink extends Component {
 
-    showEnclosureLinkModal = () => {
-        const ReactSwal = withReactContent(swal);
+    state = {
+        article: null
+    }
 
-        ReactSwal.fire({
-            title: '',
-            html: this.getEnclosureModalContent(),
-            allowOutsideClick: false,
-            allowEnterKey: false,
-            showConfirmButton: false,
-            showCancelButton: false,
-            // cancelButtonText: "Close",
-            showCloseButton: true,
-            focusCancel: true
-        })
-        .then((value) => {
-            if (value.isDismissed) {
-                this.audioElement.src = '';
-            }
+    async componentDidMount() {
+        let {article} = this.props;
+        let resolvedEnclosureUrl = await this.getResolvedUrl(article.articleEnclosureUrl);
+        article.articleEnclosureUrl = resolvedEnclosureUrl;
+
+        this.setState({
+            article: article
         });
     }
 
-    getEnclosureModalContent = () => {
-        let {article} = this.props;
+    showEnclosureLinkModal = () => {
+        if (this.state.article !== null) {
+            const ReactSwal = withReactContent(swal);
 
+            ReactSwal.fire({
+                title: '',
+                html: this.getEnclosureModalContent(),
+                allowOutsideClick: false,
+                allowEnterKey: false,
+                showConfirmButton: false,
+                showCancelButton: false,
+                showCloseButton: true,
+                focusCancel: true
+            })
+            .then((value) => {
+                if (value.isDismissed) {
+                    this.audioElement.src = '';
+                }
+            });
+        }
+    }
+
+    getEnclosureModalContent = () => {
         return (
-            <div class="divEnclosure">
-                <p>{article.articleTitle}</p>
+            <div className="divEnclosure">
+                <p>{this.state.article.articleTitle}</p>
                 <audio controls 
-                        src={article.articleEnclosureUrl}
+                        src={this.state.article.articleEnclosureUrl}
                         ref={el => this.audioElement = el} />
-                <a href={article.articleEnclosureUrl}>
+                <a href={this.state.article.articleEnclosureUrl}>
                     Download
                 </a>
             </div>
         );
+        
     }
 
-    render() {
-        
+    getResolvedUrl = async (enclosureUrl) => {        
+        let encodedUrl = encodeURIComponent(enclosureUrl);
+        let urlObject = {
+            originalUrl: encodedUrl
+        };
+
+        let request = new Request('api/rssmedia/resolvedurl', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json;charset=utf-8'
+            },                    
+            body: JSON.stringify(urlObject)
+        });
+
+        let responseJson = await fetch(request).then((response) => 
+            response.json()
+        );
+
+        return responseJson.resolvedUrl;
+    }
+
+    render() {        
         return(
             <div>
                 {this.showEnclosureLinkModal()}
