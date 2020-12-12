cb6f360bf8c5579ab68ea48b4c14d0b32c7a8514
m1k3f <41029862+mikedr0p@users.noreply.github.com>
2020-12-12 00:43:20 -0600

    navmenu settings button for modal

--- ClientApp/src/components/NavMenu/NavMenu.js
+++ ClientApp/src/components/NavMenu/NavMenu.js
@@ -1,7 +1,28 @@
 import React, { Component } from 'react';
-// import './NavMenu.css';
+import { Settings } from './modals/Settings';
 
 export class NavMenu extends Component {
+  
+  state = {
+    showSettingsModal: false
+  }
+
+  handleSettingsButtonClick = (e) => {
+    this.setState({
+      showSettingsModal: true
+    });
+  }
+
+  renderSettingsModal = (show) => {
+    let content = '';
+    if (show) {
+      content = (
+        <Settings />
+      );
+    }
+
+    return content;
+  }
 
   render () {
     return (
@@ -11,10 +32,14 @@ export class NavMenu extends Component {
           <p>Feed Reader</p>
         </div>
         <div>
+          <button onClick={this.handleSettingsButtonClick}>
+            <i className="fas fa-cog fa-2x"></i>
+          </button>
           <a href="https://github.com/m1k3f/RssMedia" target="_blank" rel="noopener noreferrer">
-              <i className="fab fa-github fa-2x"></i>
+            <i className="fab fa-github fa-2x"></i>
           </a>
-        </div>
+          {this.renderSettingsModal(this.state.showSettingsModal)}
+        </div>        
       </header>
     );
   }
