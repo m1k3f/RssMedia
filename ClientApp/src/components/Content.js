8e76f3405ba9e97d388944c67cfee39ae1ed6701
m1k3f <41029862+mikedr0p@users.noreply.github.com>
2021-02-10 15:41:33 -0600

    added help button on main

--- ClientApp/src/components/Content.js
+++ ClientApp/src/components/Content.js
@@ -1,13 +1,37 @@
 import React, { Component } from 'react';
+
+import FeedContext from './context/FeedContext';
 import { FeedBar } from './FeedBar/FeedBar';
 import { FeedArticles } from './FeedArticles/FeedArticles';
+import { HelpInfoMain } from './HelpInfoMain';
 
 export class Content extends Component {
+
+    static contextType = FeedContext;
+
+    renderItem = () => {
+        const { feedLinksSettings } = this.context;
+        let content = null;
+        if (feedLinksSettings.feedLinks.length > 0)
+        {
+            content = (
+                <FeedArticles />
+            );
+        }
+        else {
+            content = (
+                <HelpInfoMain />
+            );
+        }
+
+        return (content);
+    }
+
     render() {
         return (
             <main>                
                 <FeedBar />
-                <FeedArticles />
+                {this.renderItem()}
             </main>
         );
     }
---
+++ ClientApp/src/components/HelpInfoMain.js
@@ -0,0 +1,39 @@
+import React, { Component } from 'react';
+
+import { HelpInfo } from './modals/HelpInfo'
+
+export class HelpInfoMain extends Component {
+
+    state = {
+        showHelpModal: false
+    }
+
+    handleButtonClick = () => {
+        this.setState({
+            showHelpModal: true
+        })
+    }
+
+    renderHelpModal = () => {
+        let content = null;
+        if (this.state.showHelpModal) {
+          content = (
+            <HelpInfo />
+          );
+        }
+    
+        return (content);
+      }
+
+    render() {
+        return (
+            <div className="divHelpInfoMain">
+                <div className="divHelpInfoButton" onClick={this.handleButtonClick}>
+                    <i className="fas fa-question-circle fa-2x"></i>
+                    <p>Help</p>
+                </div>
+                {this.renderHelpModal()}
+            </div>
+        );
+    }
+}
