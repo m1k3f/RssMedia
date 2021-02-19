b54ba281cf306fd6d22f382151fbe744aa5a48fa
m1k3f <41029862+mikedr0p@users.noreply.github.com>
2021-02-19 15:19:45 -0600

    css updates

--- ClientApp/src/components/Reader.js
+++ ClientApp/src/components/Reader.js
@@ -3,13 +3,14 @@ import React, { Component } from 'react';
 import { FeedProvider } from './context/FeedContext';
 import { NavMenu } from './NavMenu/NavMenu';
 import { Content } from './Content';
+import styles from './Content.module.css';
 import '../css/rssmedia.css';
 
 export class Reader extends Component {
 
     render() {
         return (
-            <div className="fade-in">
+            <div className={`fade-in ${styles.readerApp}`}>
                 <FeedProvider>
                     <NavMenu />
                     <Content />
