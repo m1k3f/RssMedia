893d62d18f64dd4925942bbe1e1475a1fce7833b
m1k3f <41029862+mikedr0p@users.noreply.github.com>
2021-02-23 13:14:35 -0600

    helpinfo content update

--- ClientApp/src/components/modals/HelpInfo.js
+++ ClientApp/src/components/modals/HelpInfo.js
@@ -30,15 +30,14 @@ export class HelpInfo extends Component {
             <div className={styles.helpModal}>
                 <p className={styles.helpModalText} style={{fontWeight:'600'}}>Top Navigation Bar</p>                
                 <p className={styles.helpModalText}>
-                    To import a new list of feeds, click the&nbsp;<i className="fas fa-file-upload fa-lg"></i>&nbsp; 
-                    button. Imports require a .opml file, which can be exported from many other RSS readers.
+                    <i className="fas fa-file-upload fa-lg"></i>&nbsp; imports a new list of feeds 
+                    from a .opml file.
                 </p>
                 <p className={styles.helpModalText}>
-                    To export your feeds to a file, click the&nbsp;<i className="fas fa-file-download fa-lg"></i>&nbsp; 
-                    button. This file will be a .opml file, which can be imported into many other RSS readers.
+                    <i className="fas fa-file-download fa-lg"></i>&nbsp; exports your feeds to a .opml file.
                 </p>
                 <p className={styles.helpModalText}>
-                    To delete all saved feeds, click the&nbsp;<i className="fas fa-trash fa-lg"></i>&nbsp;button.
+                    <i className="fas fa-trash fa-lg"></i>&nbsp; deletes all feeds.
                 </p>
                 <p className={styles.helpModalText}>
                     All feeds are saved in web browser storage, but it is wise to back up your feeds to
@@ -47,22 +46,29 @@ export class HelpInfo extends Component {
                 <p className={styles.helpModalText}>&nbsp;</p>
                 <p className={styles.helpModalText} style={{fontWeight:'600'}}>Main Feed Bar</p>
                 <p className={styles.helpModalText}>
-                    To add a new feed, click the&nbsp;<i className="fas fa-plus fa-lg" style={{color: '#29a3a3'}}></i>&nbsp;
-                    button. You will need to provide a name and URL for the feed. The URL can either be a 
-                    direct link to an RSS feed or to a web page that has RSS feeds associated (e.g. npr.org, nytimes.com, etc). If multiple RSS
-                    feeds are found, then choose your desired feed.
+                    <i className="fas fa-plus fa-lg" style={{color: '#29a3a3'}}></i>&nbsp; adds a new feed
+                    when given a name and URL for the feed. The URL can be a 
+                    direct link to an RSS feed or a web page that has RSS feeds associated (e.g. npr.org, nytimes.com, etc). 
+                    If multiple RSS feeds are found, then choose the desired feed.
                 </p>
                 <p className={styles.helpModalText}>&nbsp;</p>
                 <p className={styles.helpModalText} style={{fontWeight:'600'}}>Feed Articles</p>
                 <p className={styles.helpModalText}>
-                    When viewing articles in a feed,&nbsp;<i className="fas fa-sync-alt fa-lg"></i> refreshes 
-                    the articles,&nbsp;<i className="fas fa-edit fa-lg"></i> edits feed details, and&nbsp;
-                    <i className="fas fa-trash fa-lg"></i> deletes the feed. 
+                    <i className="fas fa-sync-alt fa-lg"></i>&nbsp; refreshes the list of articles.
                 </p>
                 <p className={styles.helpModalText}>
-                    When viewing an individual article,&nbsp;<i className="fas fa-link fa-lg"></i>&nbsp;displays
-                    the full article in a popup window. When available,&nbsp;<i className="fas fa-paperclip fa-lg"></i>&nbsp;
-                    displays a popup window to listen to attached audio (e.g. podcasts);
+                    <i className="fas fa-edit fa-lg"></i>&nbsp; edits feed details.
+                </p>
+                <p className={styles.helpModalText}>
+                    <i className="fas fa-trash fa-lg"></i>&nbsp; deletes the feed.
+                </p>
+                <p className={styles.helpModalText}>
+                    <i className="fas fa-link fa-lg"></i>&nbsp; displays the original full article 
+                    in a popup window.
+                </p>
+                <p className={styles.helpModalText}>
+                    <i className="fas fa-paperclip fa-lg"></i>&nbsp; displays a popup window for playing  
+                    attached audio (e.g. podcasts).
                 </p>
             </div>
         );
