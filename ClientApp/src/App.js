882c524c3ede01b535d766c943d8325f8c643dbb
m1k3f <41029862+mikedr0p@users.noreply.github.com>
2021-03-13 00:04:55 -0600

    removed fontawesome css

--- ClientApp/src/App.js
+++ ClientApp/src/App.js
@@ -2,7 +2,6 @@ import React, { Component } from 'react';
 
 import {Reader} from './components/Reader';
 import './css/animation.css';
-import './css/fontawesome/all.min.css';
 
 export default class App extends Component {
 
